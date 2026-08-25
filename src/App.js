ae8c9353a2c29b645138d22019d3827b17f86409
KieuTrungHieu <kieutrunghieu104@gmail.com>
2026-08-25 11:22:59 +0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
+import { Route, Routes } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { Fragment } from "react";
+import { DefaultLayout } from "./components/Layout";
 function App() {
   return (
     <>
-      <h1>React Tiktok</h1>
+      <Routes>
+        {publicRoutes.map((route, index) => {
+          let Layout = DefaultLayout;
+          if (route.layout) {
+            Layout = route.layout
+          } else if (route.layout === null) {
+            Layout = Fragment;
+          }
+          const Page = route.component;
+          return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
+        })}
+      </Routes>
     </>
   );
 }
